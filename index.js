212b2d7bca10a49f39be126a4a04e4c2290ce503
shiweiwei <shiweiwei@menusifu.cn>
2022-02-09 17:03:44 +0800

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var body = document.body
 var loadDom = document.getElementById('load')
 
 function handleReload() {
   loadDom.style.visibility = 'visible'
+  body.style.overflow = 'hidden'
   setTimeout(function () {
     location.reload(true)
   }, 500)
